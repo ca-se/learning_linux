1022ee13af83b4b1c56f0af9c75256ff2d24c104
dantekelly <Dante.kelly97@gmail.com>
2016-03-23 00:24:35 -0400

    Updated redirect_common.py to reference signals_common.py

--- signals/redirect_signals.py
+++ signals/redirect_signals.py
@@ -19,40 +19,9 @@ def printsignal(signum, stack):
     except ValueError:
        print("That's not an int!")
     os.kill(targetpid, signum)
-    
-# Cycle through targets to find
-def cycle():
-    signal.signal(signal.SIGABRT, printsignal)
-    signal.signal(signal.SIGALRM, printsignal)
-    signal.signal(signal.SIGBUS, printsignal)
-    signal.signal(signal.SIGCHLD, printsignal)
-    signal.signal(signal.SIGCONT, printsignal)
-    signal.signal(signal.SIGFPE, printsignal)
-    signal.signal(signal.SIGHUP, printsignal)
-    signal.signal(signal.SIGILL, printsignal)
-    signal.signal(signal.SIGINT, printsignal)
-    signal.signal(signal.SIGPIPE, printsignal)
-    signal.signal(signal.SIGPOLL, printsignal)
-    signal.signal(signal.SIGPROF, printsignal)
-    signal.signal(signal.SIGQUIT, printsignal)
-    signal.signal(signal.SIGSEGV, printsignal)
-    signal.signal(signal.SIGSYS, printsignal)
-    signal.signal(signal.SIGTERM, printsignal)
-    signal.signal(signal.SIGTRAP, printsignal)
-    signal.signal(signal.SIGTSTP, printsignal)
-    signal.signal(signal.SIGTTIN, printsignal)
-    signal.signal(signal.SIGTTOU, printsignal)
-    signal.signal(signal.SIGURG, printsignal)
-    signal.signal(signal.SIGUSR1, printsignal)
-    signal.signal(signal.SIGUSR2, printsignal)
-    signal.signal(signal.SIGVTALRM, printsignal)
-    signal.signal(signal.SIGXCPU, printsignal)
-    signal.signal(signal.SIGXFSZ, printsignal)
 
 # Look back to beginning
 while True:
-    cycle()
+    signals_common.cycle()
     print('Waiting...')
     time.sleep(3)
-    
-
